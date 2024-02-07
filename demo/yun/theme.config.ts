0146008238b325b5647039cd5ee7c78ae0bf3727
kldhsh123 <123392781+kldhsh123@users.noreply.github.com>
2024-02-08 02:02:25 +0800

    Update theme.config.ts

--- demo/yun/theme.config.ts
+++ demo/yun/theme.config.ts
@@ -1,44 +1,27 @@
-import { defineThemeConfig } from 'valaxy-theme-yun'
+import { defineConfig } from 'valaxy';
+import type { ThemeConfig } from 'valaxy-theme-yun';
 
-export default defineThemeConfig({
-  // colors: {
-  //   primary: 'red',
-  // },
-  // bg_image: {},
+/**
+ * User Config
+ */
+export default defineConfig<ThemeConfig>({
+  theme: 'yun',
 
-  banner: {
-    enable: true,
-    title: '云游君的小站',
-  },
-
-  notice: {
-    enable: true,
-    content: '公告测试',
-  },
-
-  pages: [
-    {
-      name: '我的小伙伴们',
-      url: '/links/',
-      icon: 'i-ri-genderless-line',
-      color: 'dodgerblue',
-    },
-    {
-      name: '喜欢的女孩子',
-      url: '/girls/',
-      icon: 'i-ri-women-line',
-      color: 'hotpink',
-    },
-  ],
-
-  footer: {
-    since: 2016,
-    beian: {
+  themeConfig: {
+    banner: {
       enable: true,
-      icp: '苏ICP备17038157号',
+      title: '开朗的火山河',
     },
-    icon: {
-      animated: true,
+
+    pages: [
+    ],
+
+    footer: {
+      since: 2016,
+      beian: {
+        enable: true,
+        icp: '我就喜欢绕过管局不备案',
+      },
     },
   },
-})
+});
